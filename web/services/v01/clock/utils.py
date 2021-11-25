dcf06e76872ddb8d31ebdab57fe7e420c359ae3f
James Moffet <jimmoffet@gmail.com>
2021-11-24 20:01:09 -0800

    more redis fixes

--- web/services/v01/clock/utils.py
+++ web/services/v01/clock/utils.py
@@ -45,74 +45,113 @@ def usajobs():
         ('"location negotiable"', 13),
     ]
 
-    title_excludes = ['INFOSEC', 'CYBERSECURITY', 'SECURITY']
+    title_excludes = ['INFOSEC', 'CYBERSECURITY', 'SECURITY', 'physician']
 
     for search_phrase, grade in search_phrases:
-        search_param = 'Keyword=' + search_phrase + '&'
-        hiring_path_param = 'HiringPath=public&'
-        grade_param = 'PayGradeLow=' + str(grade) + '&'
-        params = search_param + hiring_path_param + grade_param
+        try:
+            search_param = 'Keyword=' + search_phrase + '&'
+            hiring_path_param = 'HiringPath=public&'
+            grade_param = 'PayGradeLow=' + str(grade) + '&'
+            params = search_param + hiring_path_param + grade_param
 
-        url = base + endpoint + params
+            url = base + endpoint + params
 
-        headers = {
-            'User-Agent': 'jimmoffet@gmail.com',
-            'Authorization-Key': os.getenv('USAJOBS_API_KEY'),
-        }
-
-        response = requests.get(url, headers=headers)
-
-        log.warning("%s",response.status_code)
-
-        json_data = response.json()
-
-        # result_dict = json_data['SearchResult']
-        result_count = json_data['SearchResult']['SearchResultCount']
-        log.warning("%s",result_count)
-        result_count_all = json_data['SearchResult']['SearchResultCountAll']
-        log.warning("%s",result_count_all)
-
-        result_array = json_data['SearchResult']['SearchResultItems']
-
-        row=1
-        for posting in result_array:
-            skip = False
-            
-            job_title = posting['MatchedObjectDescriptor']['PositionTitle']
-            
-            for exclude in title_excludes:
-                if exclude in job_title:
-                    log.warning('skipping excluded job: %s', job_title)
-                    skip = True
-                    break
-            if skip:
-                continue
-
-            # log.warning(job_title, posting['MatchedObjectDescriptor']['PositionLocationDisplay'])
-            # log.warning(posting['MatchedObjectDescriptor']['PositionLocation'])
-            # log.warning(posting['MatchedObjectDescriptor']['UserArea']['Details']['JobSummary'] )
-            duties = ''
-            for item in posting['MatchedObjectDescriptor']['UserArea']['Details']['MajorDuties']:
-                duties += item+'\n'
-            # log.warning(duties)
-            # log.warning(posting['MatchedObjectDescriptor']['QualificationSummary'])
-
-            apply_url = ''
-            if 'ApplyURI' in posting['MatchedObjectDescriptor'] and posting['MatchedObjectDescriptor']['ApplyURI']:
-                apply_url = posting['MatchedObjectDescriptor']['ApplyURI'][0]
-
-            job = {
-                'id': posting["MatchedObjectId"],
-                'title': posting['MatchedObjectDescriptor']['PositionTitle'],
-                'location': posting['MatchedObjectDescriptor']['PositionLocationDisplay'],
-                'duties': duties,
-                'qualifications': posting['MatchedObjectDescriptor']['QualificationSummary'],
-                'description': posting['MatchedObjectDescriptor']['UserArea']['Details']['JobSummary'],
-                'job_url': posting['MatchedObjectDescriptor']["PositionURI"],
-                'apply_url': apply_url
+            headers = {
+                'User-Agent': 'jimmoffet@gmail.com',
+                'Authorization-Key': os.getenv('USAJOBS_API_KEY'),
             }
-            
-            resp = write_data(job, sheet, sheetList, 'usajobs', row)
-            row+=1
-            # break
-            # log.warning(str(resp))
+
+            response = requests.get(url, headers=headers)
+
+            log.warning("%s",response.status_code)
+
+            json_data = response.json()
+
+            # result_dict = json_data['SearchResult']
+            result_count = json_data['SearchResult']['SearchResultCount']
+            log.warning("%s",result_count)
+            result_count_all = json_data['SearchResult']['SearchResultCountAll']
+            log.warning("%s",result_count_all)
+
+            result_array = json_data['SearchResult']['SearchResultItems']
+
+            row=1
+            for posting in result_array:
+                skip = False
+                job_id = 'failed to retrieve job id'
+                job_title = 'failed to retrieve job title'
+                job_location = 'failed to retrieve job location'
+                job_duties = ''
+                job_qualifications = 'failed to retrieve job qualifications'
+                job_description = 'failed to retrieve job description'
+                job_url = 'failed to retrieve job url'
+                job_apply_url = 'failed to retrieve job apply url'
+
+                try:
+                    job_id = posting['MatchedObjectId']
+                except:
+                    pass
+
+                try:
+                    job_title = posting['MatchedObjectDescriptor']['PositionTitle']
+                except:
+                    pass
+                
+                for exclude in title_excludes:
+                    if exclude in job_title:
+                        log.warning('skipping excluded job: %s', job_title)
+                        skip = True
+                        break
+                if skip:
+                    continue
+
+                try:
+                    job_location = posting['MatchedObjectDescriptor']['PositionLocationDisplay']
+                except:
+                    pass
+                
+                try:
+                    major_duties = posting['MatchedObjectDescriptor']['UserArea']['Details']['MajorDuties']
+                    for item in major_duties:
+                        job_duties += item+'\n'
+                except:
+                    pass
+                
+                try:
+                    job_qualifications = posting['MatchedObjectDescriptor']['QualificationSummary']
+                except:
+                    pass
+                
+                try:
+                    job_description = posting['MatchedObjectDescriptor']['UserArea']['Details']['JobSummary']
+                except:
+                    pass
+
+                try:
+                    job_url = posting['MatchedObjectDescriptor']["PositionURI"]
+                except:
+                    pass
+
+                try:
+                    job_apply_url = posting['MatchedObjectDescriptor']['ApplyURI'][0]
+                except:
+                    pass
+
+                job = {
+                    'id': job_id,
+                    'title': job_title,
+                    'location': job_location,
+                    'duties': job_duties,
+                    'qualifications': job_qualifications,
+                    'description': job_description,
+                    'job_url': job_url,
+                    'apply_url': job_apply_url
+                }
+                
+                resp = write_data(job, sheet, sheetList, 'usajobs', row)
+                row+=1
+                # break
+                # log.warning(str(resp))
+        except Exception as e:
+            log.error('error: %s for search phrase: %s', e, search_phrase)
+            continue
