b1597001814b4e1a7b11b10599bbb7016e4c8870
James Moffet <jimmoffet@gmail.com>
2022-02-11 23:27:43 -0800

    empty

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "s3" {
             bucket = "terraformstatebirbsbot"
-            key    = "birbsbot_terraform_state"
+            key    = "terraform/state"
             region = "us-west-2"
         }
 }
@@ -35,20 +35,31 @@ resource "aws_s3_bucket" "s3Bucket" {
      policy  = <<EOF
     {
         "id" : "MakePublic",
-    "version" : "2012-10-17",
-    "statement" : [
+    "Version": "2012-10-17",
+    "Statement": [
         {
-            "action" : [
-                "s3:GetObject"
-            ],
-            "effect" : "Allow",
-            "resource" : "arn:aws:s3:::terraformstatebirbsbot/*",
-            "principal" : "*"
+        "Effect": "Allow",
+        "Action": "s3:ListBucket",
+        "Resource": "arn:aws:s3:::mybucket"
+        },
+        {
+        "Effect": "Allow",
+        "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+        "Resource": "arn:aws:s3:::terraformstatebirbsbot/terraform/state"
         }
-        ]
+    ]
     }
     EOF
     website {
        index_document = "index.html"
    }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "terraformstatebirbsbot"
+    key    = "network/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
