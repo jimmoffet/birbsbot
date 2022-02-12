6875ba992fc445ed85a37ee5c5c75534b066c7a4
James Moffet <jimmoffet@gmail.com>
2022-02-11 21:30:28 -0800

    terraform init

---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+  backend "s3" {
+            bucket = var.bucket_name
+            key    = birbsbot_terraform_state
+            region = "us-west-2"
+        }
+    }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_s3_bucket" "s3Bucket" {
+     bucket = var.bucket_name
+     acl       = "public-read"
+
+     policy  = <<EOF
+    {
+        "id" : "MakePublic",
+    "version" : "2012-10-17",
+    "statement" : [
+        {
+            "action" : [
+                "s3:GetObject"
+            ],
+            "effect" : "Allow",
+            "resource" : "arn:aws:s3:::terraform_state_bucket/*",
+            "principal" : "*"
+        }
+        ]
+    }
+    EOF
+    website {
+       index_document = "index.html"
+   }
+}
