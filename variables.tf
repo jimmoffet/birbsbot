8aa8ca5fce56b188d3ec9100e064d4b81053575e
James Moffet <jimmoffet@gmail.com>
2022-02-11 22:02:11 -0800

    terraform updates

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "ExampleAppServerInstance"
+  default     = "birbsbot"
 }
 
 variable "bucket_name" {
   description = "Value of the bucket name for storing state on s3"
   type        = string
-  default     = "terraform_state_bucket"
+  default     = "terraformstatebirbsbot"
 }
